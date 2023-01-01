e19abfcb829eb8816abaa5808e1f43de5601bbb7
dan <danilaoferraoalves@gmail.com>
2023-01-01 16:13:41 -0300

    Portfólio

---
+++ main.js
@@ -0,0 +1,66 @@
+var btn = document.getElementById('mobile')
+btn.addEventListener('click', togglemenu)
+
+function togglemenu() { //menu mobile
+    var nav = document.getElementById('nav')
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.toggle('active')
+    hamburguer.classList.toggle('active')
+
+}
+
+const btnprojeto = document.getElementById('btnprojeto')
+btnprojeto.addEventListener('click', toggleprojetos)
+
+// habilitar os projetos
+function toggleprojetos() {
+    const projetoshidden = document.getElementById('vermais')
+    projetoshidden.classList.toggle('mostrar')
+    if(projetoshidden.classList.contains('mostrar')) {
+        btnprojeto.innerText = 'Ver Menos'
+    } else {
+        btnprojeto.innerText = 'Ver Mais'
+        window.location.href = '#projetos'
+    }
+}
+
+var link = document.querySelector('.link')
+var link2 = document.querySelector('.link2')
+var link3 = document.querySelector('.link3')
+var link4 = document.querySelector('.link4')
+var link5 = document.querySelector('.link5')
+
+link.addEventListener('click', function() {
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.remove('active')
+    hamburguer.classList.remove('active')
+})
+link2.addEventListener('click', function() {
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.remove('active')
+    hamburguer.classList.remove('active')
+})
+link3.addEventListener('click', function() {
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.remove('active')
+    hamburguer.classList.remove('active')
+})
+link4.addEventListener('click', function() {
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.remove('active')
+    hamburguer.classList.remove('active')
+})
+link5.addEventListener('click', function() {
+    var hamburguer = document.getElementById('mobile')
+    nav.classList.remove('active')
+    hamburguer.classList.remove('active')
+})
+
+function loading() {
+
+    setTimeout(() => {
+        document.getElementsByClassName('box-load')[0].style.display = "none"
+        document.getElementsByClassName('allcontent')[0].style.display = "block"
+    }, 3000)
+
+}
